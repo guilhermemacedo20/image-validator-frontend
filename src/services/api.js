212e5a83e055300ff25936f6d79169b426beebae
Guilherme Macedo <guilhermeap.macedo20@gmail.com>
2026-04-09 13:57:29 -0300

    fix: adjust route

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,17 @@
+import axios from 'axios'
 import { environment } from '../config/environment'
+
+export const api = axios.create({
+  baseURL: environment.backend.url,
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 
 api.interceptors.response.use(
   (res) => res,
